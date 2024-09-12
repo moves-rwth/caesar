7ac8e8531e792e974e2308e6fa19463b8508f1cd
umutdural <umuty.dural@gmail.com>
2024-09-12 20:50:58 +0200

    vscode-ext: status bar working command links

--- vscode-ext/src/StatusBarComponent.ts
+++ vscode-ext/src/StatusBarComponent.ts
@@ -5,12 +5,25 @@ import { ServerStatus, VerifyResult } from "./CaesarClient";
 import { DocumentMap, Verifier } from "./Verifier";
 import { ConfigurationConstants } from "./constants";
 
+
+const runningTooltipBase =
+    new vscode.MarkdownString(
+        "[Stop Caesar](command:caesar.stopServer)\n\n" +
+        "[Restart Caesar](command:caesar.restartServer)"
+        , true);
+
+const stoppedTooltipBase =
+    new vscode.MarkdownString(
+        "[Start Caesar](command:caesar.startServer)", true);
+
 export class StatusBarComponent {
 
     private enabled: boolean;
     private verifyStatus: DocumentMap<[Range, VerifyResult][]> = new DocumentMap();
     private serverStatus: ServerStatus = ServerStatus.NotStarted;
     private view: StatusBarItem;
+    private tooltipBase: vscode.MarkdownString = stoppedTooltipBase;
+    private tooltipExtension: vscode.MarkdownString = new vscode.MarkdownString();
 
     constructor(verifier: Verifier) {
         // create the view
@@ -19,11 +32,6 @@ export class StatusBarComponent {
         this.view.text = "Caesar";
         this.view.command = "caesar.showOutput";
 
-        this.view.tooltip = new vscode.MarkdownString(
-            "<a href='command:caesar.restartServer'>Restart Caesar</a>\n\n" +
-            "[Start Caesar](command:caesar.startServer)\n\n" +
-            "[Stop Caesar](command:caesar.stopServer)",
-            true);
 
         // render if enabled
         this.enabled = StatusBarViewConfig.get(ConfigurationConstants.showStatusBar);
@@ -52,18 +60,22 @@ export class StatusBarComponent {
 
     render() {
         if (this.enabled) {
+
             switch (this.serverStatus) {
                 case ServerStatus.NotStarted:
                     this.view.text = "$(debug-start) Caesar Inactive";
                     this.view.command = "caesar.startServer";
+                    this.tooltipBase = stoppedTooltipBase;
                     break;
                 case ServerStatus.Stopped:
                     this.view.text = "$(debug-start) Et tu, Brute?";
                     this.view.command = "caesar.startServer";
+                    this.tooltipBase = stoppedTooltipBase;
                     break;
                 case ServerStatus.FailedToStart:
                     this.view.text = "$(warning) Failed to start Caesar";
                     this.view.command = "caesar.startServer";
+                    this.tooltipBase = stoppedTooltipBase;
                     break;
                 case ServerStatus.Starting:
                     this.view.text = "$(loading~spin) Starting Caesar...";
@@ -72,34 +84,68 @@ export class StatusBarComponent {
                 case ServerStatus.Ready:
                     this.view.text = "$(thumbsup-filled) Caesar Ready";
                     this.view.command = "caesar.showOutput";
+                    this.tooltipBase = runningTooltipBase;
                     break;
                 case ServerStatus.Verifying:
                     this.view.text = "$(sync~spin) Verifying...";
                     this.view.command = ""
                     break;
                 case ServerStatus.Finished:
-                    this.handleFinishedStatus()
-                    // If no errors are present, show the verified status
-                    // this.view.text = "$(check) Verified";
+                    this.tooltipBase = runningTooltipBase;
+                    this.handleFinishedStatus();
                     this.view.command = "caesar.showOutput";
                     break;
             }
+
+            this.renderTooltip();
             this.view.show();
         } else {
             this.view.hide();
         }
     }
 
-    private handleFinishedStatus() {
-        let tooltipString = new vscode.MarkdownString("");
-        tooltipString.supportThemeIcons = true;
-        tooltipString.supportHtml = true;
+    private extendedTooltip(extension: vscode.MarkdownString) {
+        return new vscode.MarkdownString(this.tooltipBase.value + "\n\n --- \n" + extension.value, true);
+    }
 
-        let everythingVerified = true;
+    private renderTooltip() {
+        if (this.tooltipExtension.value === "") {
+            this.view.tooltip = this.tooltipBase;
+            this.view.tooltip.isTrusted = true;
+            return;
+        }
+        this.view.tooltip = this.extendedTooltip(this.tooltipExtension);
+        this.view.tooltip.isTrusted = true;
+    }
 
+    private countResults(results: [Range, VerifyResult][]): [number, number, number] {
         let verified = 0;
         let failed = 0;
         let unknown = 0;
+
+        for (const [_, result] of results) {
+            switch (result) {
+                case VerifyResult.Verified:
+                    verified++;
+                    break;
+                case VerifyResult.Failed:
+                    failed++;
+                    break;
+                case VerifyResult.Unknown:
+                    unknown++;
+                    break;
+            }
+        }
+
+        return [verified, failed, unknown];
+    }
+
+
+    private handleFinishedStatus() {
+        let tooltipString = new vscode.MarkdownString("", true);
+
+        let everythingVerified = true;
+
         for (const [document_id, results] of this.verifyStatus.entries()) {
             for (const editor of vscode.window.visibleTextEditors) {
 
@@ -111,42 +157,25 @@ export class StatusBarComponent {
                     continue;
                 }
 
-                verified = 0;
-                failed = 0;
-                unknown = 0;
-
-                for (const [_, result] of results) {
-
-                    switch (result) {
-                        case VerifyResult.Verified:
-                            verified++;
-                            break;
-                        case VerifyResult.Failed:
-                            failed++;
-                            break;
-                        case VerifyResult.Unknown:
-                            unknown++;
-                            break;
-                    }
-                }
+                let [verified, failed, unknown] = this.countResults(results);
 
                 if (failed > 0 || unknown > 0) {
                     everythingVerified = false;
                 }
 
-                tooltipString.appendMarkdown(`${vscode.Uri.parse(document_id.uri).path}: $(error) ${failed} $(question) ${unknown}`);
-                tooltipString.appendText("\n");
-                tooltipString.appendMarkdown("---");
-                tooltipString.appendText("\n");
+                tooltipString.appendMarkdown(`${vscode.Uri.parse(document_id.uri).path}: $(error) ${failed} $(question) ${unknown}` + "\n\n --- \n");
             }
         }
 
+        // Remove the last newline and separator
+        tooltipString.value.trimEnd();
+
         if (everythingVerified) {
             this.view.text = "$(pass) Verified!";
-            this.view.tooltip = new vscode.MarkdownString("No errors found", true);
+            this.tooltipExtension = new vscode.MarkdownString("No errors found", true);
         } else {
             this.view.text = "$(warning) Verification Errors";
-            this.view.tooltip = tooltipString;
+            this.tooltipExtension = tooltipString;
         }
 
     }
